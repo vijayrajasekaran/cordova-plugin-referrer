b9bf4c3155b1fae37bacce9e226f25f336d44db8
Vijay Rajasekaran <vijayr2002@gmail.com>
2017-06-22 17:51:46 +0530

    Initial commit

---
+++ src/Receiver.java
@@ -0,0 +1,26 @@
+package com.install.referral;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class Receiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        //Get referrer
+        String referrer = intent.getStringExtra("referrer");
+
+        //Save Referrer Info from SharedPreferences
+
+        SharedPreferences sharedpreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor edit = sharedpreferences.edit();
+        edit.putString("referrer", referrer);
+        edit.apply();
+		
+    }
+}
